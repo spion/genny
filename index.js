70dc95c3b090646c90a0bb888afe48c87fa9cb3b
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-06-12 15:30:25 +0200

    first functional and fully tested version

--- index.js
+++ index.js
@@ -1,75 +1,74 @@
 function genny(gen) {
-  return function start() {
-    var args = [].slice.call(arguments);
+    return function start() {
+        var slice = [].slice;
+        var args = slice.call(arguments);
 
-    if (args.length <= 1) 
-      callback = null;
-    else 
-      var callback = args[args.length - 1];
-      if (!(callback instanceof Function))
-        callback = null;
+        if (args.length < 1) 
+            callback = null;
+        else 
+            var callback = args[args.length - 1];
+        if (!(callback instanceof Function))
+            callback = null;
 
-    var iterator;
-    var nextYields = [];
+        var iterator;
+        var nextYields = [];
 
-    function sendNextYield() {
-      while (nextYields.length) { 
-        console.log(nextYields);
-        var ny = nextYields.pop();
-        console.log("sending queued yield", ny);
-        iterator.send(ny);
-      }
-    }
-
-
-    var resume = createResumer.bind(null, true);
-
-
-    function createResumer(throwing) {
-      var called = false;
-      var sliceArgs = throwing ? 1 : 0;
-      return function resume(err) {
-        if (called) return;
-        called = true;
-
-        if (err && throwing) try {
-          return iterator.throw(err);
-        } catch (e) {
-          // if we have a callback passed, send it the error
-          if (callback) callback(e);
-          // otherwise throw.
-          else throw e;
-
-        } else {
-          var sendargs = Array.prototype.slice.call(arguments, sliceArgs);
-          if (sendargs.length <= 1) 
-            sendargs = sendargs[0];
-          try {
-            iterator.send(sendargs);
-            sendNextYield();
-          } catch (e) { // already running
-            nextYields.push(sendargs); // delay send
-          }
+        function sendNextYield() {
+            while (nextYields.length) {         
+                var ny = nextYields.pop();
+                iterator.send(ny);
+            }
         }
 
-      }
+
+        var resume = createResumer.bind(null, true);
+        var resumerId = 0;
+
+        function createResumer(throwing) {
+            var called = false;
+            // If its not a throwing resumer, dont slice the error argument
+            var sliceArgs = throwing ? 1 : 0;
+            var rid = ++resumerId;
+            return function resume(err) {
+                if (called) return;
+                called = true;
+                if (err && throwing) try {
+                    return iterator.throw(err);
+                } catch (e) {
+                    // if we have a callback passed, send it the error
+                    if (callback) callback(e);
+                    // otherwise throw.
+                    else throw e;
+
+                } else {
+                    var sendargs = slice.call(arguments, sliceArgs);
+                    if (sendargs.length <= 1) {
+                        sendargs = sendargs[0];
+                    }
+                    try {
+                        iterator.send(sendargs);
+                        sendNextYield();
+                    } catch (e) { // already running, delay send
+                        nextYields.push(sendargs);
+                    }
+                }
+
+            }
+        }
+
+        Object.defineProperty(resume, 'throw', {
+            get: createResumer.bind(null, true) 
+        });
+
+        Object.defineProperty(resume, 'nothrow', {
+            get: createResumer.bind(null, false) 
+        });
+
+        args.unshift(resume);
+        iterator = gen.apply(this, args);
+        iterator.next();
+        sendNextYield();
     }
-
-    Object.defineProperty(resume, 'throw', {
-      get: createResumer.bind(null, false) 
-    });
-
-    Object.defineProperty(resume, 'nothrow', {
-      get: createResumer.bind(null, true) 
-    });
-
-
-    args.unshift(resume);
-
-    iterator = gen.apply(this, args);
-    iterator.next();
-    sendNextYield();
-  }
 }
 
 module.exports = genny;
--- test/index.js
+++ test/index.js
@@ -3,65 +3,89 @@ var t = require('tap');
 var genny = require('../');
 
 function errors(cb) {
-  setImmediate(cb.bind(this, new Error('oops')));
+    setImmediate(cb.bind(this, new Error('oops')));
 }
 
 function nowait(k, cb) {
-  cb(null, k);
+    cb(null, k);
 }
 
 function evil(cb) {
-  setTimeout(cb, 10);
-  setImmediate(cb);
+    setTimeout(cb, 10);
+    setImmediate(cb);
 }
 
 function normal(cb) {
-  setTimeout(cb.bind(null, "OK"), 20);
+    setTimeout(cb.bind(this, null, "OK"), 20);
+}
+
+function multiresult(cb) {
+    setImmediate(cb.bind(this, null, 'r1', 'r2'));
 }
 
 t.test(
-  "simple test", 
-  genny(function* (resume, t) {
-    yield setImmediate(resume.throw);
-    t.end();    
-  }));
-
-t.test(
-  "throws error", 
-  genny(function* (resume, t) {
-    try {
-      yield errors(resume.throw);
-    } catch (e) {
-      t.ok(e, "error was thrown");
-      t.end();
-    }
-  }));
-
-t.test(
-  "calls callback if present instead of throwing", 
-  function(t) { 
+    "simple test", 
     genny(function* (resume, t) {
-      yield errors(resume());
-    })(function(err) {
-      t.ok(err, "error present");
-      t.end();
-    })
-  });
+        yield setImmediate(resume.throw);
+        t.ok(true, 'resume success');
+        t.end();    
+    }));
+
+t.test(
+    "throws error", 
+    genny(function* (resume, t) {
+        try {
+            yield errors(resume.throw);
+        } catch (e) {
+            t.ok(e, "error was thrown");
+            t.end();
+        }
+    }));
+
+t.test(
+    "calls callback if present instead of throwing", 
+    function(t) { 
+        genny(function* (resume, t) {
+            yield errors(resume.throw);
+        })(function(err) {
+            t.ok(err, "error present");
+            t.end();
+        })
+    });
 
 
 t.test(
-  "handles nowait in a loop",
-  genny(function* (resume, t) { 
-    for (var k = 0; k < 3; ++k)
-      yield nowait(k, resume.throw);
-    t.end();
-  }));
+    "handles nowait in a loop",
+    genny(function* (resume, t) { 
+        for (var k = 0; k < 10; ++k)
+        yield nowait(k, resume.throw);
+        t.ok(true, 'resumed all nowait yields');
+        t.end();
+    }));
 
 t.test(
-  "handles evil functions that run callbacks multiple times",
-  genny(function* (resume, t) {
-    yield evil(resume);
-    var res = yield normal(resume.throw);
-    t.equals(res, "OK");
-  }));
+    "handles evil functions that run callbacks multiple times",
+    genny(function* (resume, t) {
+        yield evil(resume.throw);
+        var res = yield normal(resume.throw);
+        t.equals(res, "OK", 'got result from non-evil function');
+        t.end();
+    }));
 
+t.test(
+    "supports multi-result functions",
+    genny(function* (resume, t) {
+        var res = yield multiresult(resume.throw);
+        t.equals(res[0], 'r1', 'first result is there');
+        t.equals(res[1], 'r2', 'second result is there');
+        t.end();
+    }));
+
+t.test(
+    "supports no-throw variant",
+    genny(function* (resume, t) {
+        var res = yield multiresult(resume.nothrow);
+            t.equals(res[0], null, 'first argument is error');
+            t.equals(res[2], 'r2', 'third argument is r2');
+            t.end();
+        }));
