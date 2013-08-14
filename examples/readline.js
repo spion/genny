699fd03d0fc803ebc1f68a9642ea5efa49b08c41
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-08-14 13:56:07 +0200

    Simplified example

--- examples/readline.js
+++ examples/readline.js
@@ -1,4 +1,3 @@
-
 var getLine = genny.fn(function* (resume, file, number) {
     var data = yield fs.readFile(file, resume.t);
     return data.toString().split('\n')[number];
@@ -13,7 +12,6 @@ getLine('test.js', 2, function(err, lineContent) {
 
 
 exports.createWithUserAccount = genny.fn(function *(resume, newAccount) {
-    console.log('account to be saved:', newAccount);
 
     yield exports.create({
         accountType: newAccount.accountType,
@@ -28,16 +26,16 @@ exports.createWithUserAccount = genny.fn(function *(resume, newAccount) {
         companyName: newAccount.companyName
     });
     var userAccount = yield UserAccount.create({
-            address: address,
-            userId: newAccount.userId,
-            displayName: newAccount.displayName,
-            accountId: account[0].id
-        }, resume.t); 
+        address: address,
+        userId: newAccount.userId,
+        displayName: newAccount.displayName,
+        accountId: account[0].id
+    }, resume.t); 
 
     yield AccountAdmin.create({
         accountId: userAccount[0].accountId,
         userId: userAccount[0].userId
     }, resume.t); 
 
-    return userAccount[0].id;
+    return userAccount.id;
 }
