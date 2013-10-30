da109ec1078ac840b216dc53a38540267bec72ea
Jim Radford <radford@blackbean.org>
2013-10-30 09:50:58 -0700

    Change WorkQueue/WorkItem to be doubly linked

--- test/workqueue.js
+++ test/workqueue.js
@@ -6,12 +6,58 @@ var wq = require('../lib/work-queue'),
 
 
 t.test('work queue', function (t) {
-    var q = new WorkQueue();
-    q.add(new WorkItem('a'));
+    var q = new WorkQueue(), a = new WorkItem('a');
+
+    t.equals(q.next, q);
+    t.equals(q.prev, q);
+
+    q.add(a);
     t.equals(q.advance(), undefined);
+
+    t.equals(q.next, a);
+    t.equals(q.prev, a);
+
     q.next.complete = true;
+    t.equals(q.advance(), a);
+    t.equals(q.advance(), undefined);
+
+    t.equals(q.next, q);
+    t.equals(q.prev, q);
+    t.end();
+})
+
+t.test('work queuing', function (t) {
+    var q = new WorkQueue(), a = new WorkItem('a'), b = new WorkItem('b'), c = new WorkItem('c'), d = new WorkItem('d');
+
+    t.equals(q.advance(), undefined);
+
+    q.add(a);
     t.equals(q.next.value, 'a');
-    q.advance()
-    t.equals(q.next, null);
+    t.equals(q.advance(), undefined);
+
+    q.add(b);
+    t.equals(q.advance(), undefined);
+
+    q.add(c);
+    t.equals(q.advance(), undefined);
+
+    b.complete = true;
+    t.equals(q.advance(), undefined);
+
+    a.complete = true;
+    t.equals(q.advance(), a);
+    t.equals(q.advance(), b);
+    t.equals(q.advance(), undefined);
+
+    c.complete = true;
+    t.equals(q.advance(), c);
+    t.equals(q.advance(), undefined);
+
+    q.add(d);
+    t.equals(q.advance(), undefined);
+    d.complete = true;
+    t.equals(q.advance(), d);
+    t.equals(q.advance(), undefined);
+
     t.end();
 })
